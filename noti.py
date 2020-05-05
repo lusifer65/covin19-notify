f2f68837204ccfc339a8cd3659f551b773cbf915
Akash verma <42277681+lusifer65@users.noreply.github.com>
2020-05-05 13:08:14 -0400

    Update noti.py

--- noti.py
+++ noti.py
@@ -19,18 +19,9 @@ while(True):
         st+=tr.get_text()
     st=st[1:]
     ilist=st.split("\n\n")
-    # print("enter the no of state")
-    # for item in ilist[:32]:
-    #     x = item.split('\n')
-    #     print(f"{x[0]} {x[1]}")
-    # i=input()
-    state=['Uttar Pradesh','Delhi',]
+    state=['Uttar Pradesh','Delhi',] #add state 
     for item in ilist[:33]:
         x=item.split('\n')
-        # print(x[1]+"\tcase:-"+x[2]+"\tdead:-"+x[4])
-        if(x[1]=='Uttar Pradesh'):
+        if(x[1] in state):
             notification_sys("covin_19 cases in "+x[1],f"\n cases:{x[2]} Cured:{x[3]} dead:{x[4]}")
-            # notification_sys("covin_19 cases in "+x[1],x[1]+"\tcase:-"+x[2]+"\tCured"+x[3]+"\tdead:-"+x[4])
-        # if (x[0] == 'Total number of confirmed cases in India'):
-        #     notification_sys("covin_19 cases in " + x[1], f"\n cases:{x[2]} Cured:{x[3]} dead:{x[4]}")
-    time.sleep(2)
+    time.sleep(120) #chage time in sec 
